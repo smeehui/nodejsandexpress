cc08db959def3bda00c91e72efc56e1d27aaa3e6
smeehui <nguyenvanquanghuy99@gmail.com>
2022-09-24 12:16:32 +0700

    add views

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import morgan from "morgan";
+import { engine } from "express-handlebars";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(morgan("tiny"));
+
+app.engine("hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resource", "views"));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/news", (req, res) => res.render("news"));
+app.listen(port, (req, res) => console.log(`Listening on ${port}`));
